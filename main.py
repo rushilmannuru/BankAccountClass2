ed770c3e6cd2f83730254623f172f2e7d4f5683e
rushilmannuru <rushil.mannuru@gmail.com>
2026-09-25 18:31:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from CheckingAccount import Checking
 
 def main():
     # Scenario
-    checking = BankAccount("Alice", 500, 100, "CHECK001")
+    checking = BankAccount("Juice", 500, 100, "CHECK001")
     checking2 = Checking("Leo", 800, 150, "CHECK002", 100)
     checking.print_customer_information()
 
@@ -21,7 +21,7 @@ def main():
     checking.withdraw(5000)
     print(checking.current_balance)
 
-    savings = Savings("Alice", 1000, 200, "SAVING001", 0.05)
+    savings = Savings("Juice", 1000, 200, "SAVING001", 0.05)
     savings2 = Savings("Leo", 1500, 300, "SAVING002", 0.05)
     savings.print_customer_information()
 
