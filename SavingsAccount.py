79fb015adb417d2a230bd46e09c7cb1bc09c976d
Niko <yaneznikolas@gmail.com>
2026-09-24 21:43:52 -0400

    Added SavingsAccount class

---
+++ SavingsAccount.py
@@ -0,0 +1,13 @@
+from BankAccountClassMain import BankAccount
+
+class Savings(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self.interest_rate = interest_rate
+
+    def add_interest(self):
+        self.current_balance += self.current_balance * self.interest_rate
+
+    def print_customer_information(self):
+        super().print_customer_information()
+        print(self.interest_rate)
