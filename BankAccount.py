886bb25340a4db4369cc9c2e1aeea4a656d5d72a
rushilmannuru <rushil.mannuru@gmail.com>
2026-09-25 18:18:26 -0400

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -6,7 +6,12 @@ class BankAccount:
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
         self.__account_number = account_number # private
+        self._routing_number = routing_number #protected
 
+    @property
+    def routing_number(self):
+        return self._routing_number
+        
     @property
     def account_number(self):
         return self.__account_number
@@ -25,5 +30,6 @@ class BankAccount:
         print("Bank: "+BankAccount.bank_title)
         print("Customer Name: " + self.customer_name)
         print(f"Account Number: {self.__account_number}")
+        print(f"Routing Number: {self._routing_number}")
         print(f"Current Balance: {self.current_balance}")
         print(f"Minimum Balance: {self.minimum_balance}")
